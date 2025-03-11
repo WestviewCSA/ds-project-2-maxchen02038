d07a02df37814d284c88069031f25c900b42eecb
1902038 <1902038@01-18118.pusd.dom>
2025-03-11 14:10:30 -0700

    created tile class and worked on p2

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,9 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class p2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("p2");
+		readMap("Test01");
 	}
 
+	
+	public static void readMap(String fileName) {
+		try {
+			File file = new File(fileName);
+			Scanner scanner = new Scanner(file);
+			int numRows = scanner.nextInt();
+			int numCols = scanner.nextInt();
+			int numRooms = scanner.nextInt();
+			
+			int rowIndex=0;
+			while(scanner.hasNextLine()) {
+				String row = scanner.nextLine();
+				for(int i =0; i<numCols&& i<row.length(); i++) {
+					char el = row.charAt(i);
+					Tile obj = new Tile(rowIndex, i, el );
+				}
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
+		
+	}
+	
 }
+
+
